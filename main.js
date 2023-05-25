a80f96ff7c9030954977b45bbfed31015ff8f72d
Tanmayyy-11 <100669453+Tanmayyy-11@users.noreply.github.com>
2023-05-26 02:03:05 +0530

    Added farthest point function

--- main.js
+++ main.js
@@ -1,56 +1,88 @@
 import * as THREE from 'three';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 500 );
-// const camera = new THREE.OrthographicCamera(1000,-1000 ,1000, -1000 , 1, 1000);
-const light = new THREE.DirectionalLight(0x0000f0, 10);
-light.position.set(0,0,30);
-// light.target.mesh(cube);
-scene.add(light);
+let camera, scene, renderer;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+init();
+render();
 
-const geometry = new THREE.BoxGeometry( 2, 2, 2 );
-const material = new THREE.MeshBasicMaterial( { color: 0xfff000  } );
-const material1 = new THREE.MeshBasicMaterial( { color: 0x000fff } );
-const cube = new THREE.Mesh( geometry, material );
+function init() {
 
-const cube2 = new THREE.Mesh(geometry, material1);
-cube.position.x = -2.5;
-cube2.position.x +=2.5;
-scene.add(cube);
+  camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+  camera.position.z = 2;
 
-scene.add(cube2);
+  scene = new THREE.Scene();
 
-camera.position.z = 25;
-camera.position.x = -5;
-camera.position.y = 5;
-camera.updateProjectionMatrix;
-function animate() {
-	requestAnimationFrame( animate );
+  const vertices = [
+    0, 0, 0,
+    1, 0, 0,
+    1, 1, 0,
+    0, 1, 0
+  ];
+  const vertices1 = [
+    0.5, 0.5, 0,
+    0.5, 1.5, 0,
+    -0.5, 1.5, 0,
+    -0.5, 0.5, 0
+  ];
 
-	//cube.rotation.x += 0.01;
-	//cube.rotation.y += 0.01;
-	cube.translateX(0.01);
-	
-	cube2.translateX(-0.01);
-	//cube2.rotation.x += 0.01;
-	//cube2.rotation.y += 0.01;
-	renderer.render( scene, camera );
+  const material = new THREE.MeshBasicMaterial( { color: 0xfff000  } );
+  const material1 = new THREE.MeshBasicMaterial({ color: 0x000fff });
+
+  const myshape = createQuad(vertices,material1);
+  scene.add(myshape);
+
+  const shape2 = createQuad(vertices1,material)
+  scene.add(shape2);
+
+  renderer = new THREE.WebGLRenderer({antialias: true});
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+ 
 }
 
-import WebGL from 'three/addons/capabilities/WebGL.js';
+function render() {
 
-if ( WebGL.isWebGLAvailable() ) {
+  renderer.render(scene, camera);
 
-	// Initiate function or other initializations here
-	animate();
+}
 
-} else {
+function getFarthestPoint(shape, direction) {
+	const geometry = shape.geometry;
+	const positionAttribute = geometry.getAttribute('position');
+	const vertices = positionAttribute.array;
+	const numVertices = vertices.length;
+  
+	if (numVertices === 0) {
+	  return null; // No vertices in the shape
+	}
+    let newvertex = new THREE.Vector3(vertices[0],vertices[1],vertices[2]);
+	let farthestPoint = new THREE.Vector3();
+	farthestPoint.copy(newvertex).applyMatrix4(shape.matrixWorld);
+	let maxProjection = farthestPoint.dot(direction);
+  
+	for (let i = 3; i < numVertices; i+=3) {
+		let newvertex = new THREE.Vector3(vertices[i],vertices[i+1],vertices[i+2]);
+		let vertex = new THREE.Vector3();
+		vertex.copy(newvertex).applyMatrix4(shape.matrixWorld);
+		let projection = vertex.dot(direction);
+		if(projection > maxProjection){
+			farthestPoint = vertex;
+			maxProjection = projection;
+	  	}
+	}
+	return farthestPoint;
 
-	const warning = WebGL.getWebGLErrorMessage();
-	document.getElementById( 'container' ).appendChild( warning );
+}
 
+function createQuad(vertices,material){
+	const geometry = new THREE.BufferGeometry();
+	const indices = [
+		0, 1, 2, // first triangle
+		2, 3, 0 // second triangle
+	  ];
+	  geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+	  geometry.setIndex(indices);
+	  const myshape = new THREE.Mesh(geometry, material);
+	  return myshape;
 }
