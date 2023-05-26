fa213df6e5b94cd623a125905ea2c91797ae5395
Tanmayyy-11 <100669453+Tanmayyy-11@users.noreply.github.com>
2023-05-26 14:34:46 +0530

    Update main.js
    algo implemented but errors exist

--- main.js
+++ main.js
@@ -5,7 +5,11 @@ let camera, scene, renderer;
 init();
 render();
 
-function init() {
+
+
+
+function init()
+{
 
   camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
   camera.position.z = 2;
@@ -19,10 +23,10 @@ function init() {
     0, 1, 0
   ];
   const vertices1 = [
-    0.5, 0.5, 0,
-    0.5, 1.5, 0,
-    -0.5, 1.5, 0,
-    -0.5, 0.5, 0
+    0, -1.2, 0,
+    1, -1.2, 0,
+    1, -0.2, 0,
+    0, -0.2, 0
   ];
 
   const material = new THREE.MeshBasicMaterial( { color: 0xfff000  } );
@@ -34,6 +38,13 @@ function init() {
   const shape2 = createQuad(vertices1,material)
   scene.add(shape2);
 
+//   let direction = new THREE.Vector3(1,0,1);
+//   console.log(getFarthestPoint(myshape,direction));
+
+  console.log(gjk(myshape,shape2));
+  
+
+
   renderer = new THREE.WebGLRenderer({antialias: true});
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
@@ -41,13 +52,15 @@ function init() {
  
 }
 
-function render() {
+function render()
+{
 
   renderer.render(scene, camera);
 
 }
 
-function getFarthestPoint(shape, direction) {
+function getFarthestPoint(shape, direction)
+{
 	const geometry = shape.geometry;
 	const positionAttribute = geometry.getAttribute('position');
 	const vertices = positionAttribute.array;
@@ -71,11 +84,13 @@ function getFarthestPoint(shape, direction) {
 			maxProjection = projection;
 	  	}
 	}
+	console.log(shape,farthestPoint,direction);
 	return farthestPoint;
 
 }
 
-function createQuad(vertices,material){
+function createQuad(vertices,material)
+{
 	const geometry = new THREE.BufferGeometry();
 	const indices = [
 		0, 1, 2, // first triangle
@@ -85,4 +100,134 @@ function createQuad(vertices,material){
 	  geometry.setIndex(indices);
 	  const myshape = new THREE.Mesh(geometry, material);
 	  return myshape;
+}
+
+function gjk(shape1,shape2)
+{
+
+	let centre1 = find_centre(shape1);
+	let centre2 = find_centre(shape2);
+	let d = subvec(centre1,centre2);
+	console.log(centre1,centre2);
+	let simplex = [support(shape1,shape2,d)];
+	console.log(simplex[0]);
+	let origin  =  new THREE.Vector3(0,0,0);
+	d = subvec(origin,simplex[0]);
+	console.log(d);
+	while(1){
+		let A = support(shape1,shape2,d);
+		console.log(A);
+		if(A.dot(d)<0)
+		{
+			return false;
+		}
+		simplex.push(A);
+		let obj = {simplex1 : simplex, d1  : d};
+		if(handleSimplex(obj)){
+			return true;
+		}
+	}
+
+}
+
+
+function find_centre(shape)
+{
+
+	const geometry1 = shape.geometry;
+	const positionAttribute1 = geometry1.getAttribute('position');
+	const vertices1 = positionAttribute1.array;
+	const numVertices1 = vertices1.length;
+	let newx=0,newy=0,newz=0;
+	for( let i=0;i<numVertices1;i+=3){
+		newx+=vertices1[i];
+		newy+=vertices1[i+1];
+		newz+=vertices1[i+2];
+	}
+    let centre = new THREE.Vector3(newx*3/numVertices1,newy*3/numVertices1,newz*3/numVertices1);
+	return centre;	
+}
+
+function support(shape1,shape2,direction)
+{	
+	let origin  =  new THREE.Vector3(0,0,0);
+	return subvec(getFarthestPoint(shape1,direction),getFarthestPoint(shape2,subvec(origin,direction)));
+}
+
+
+
+function handleSimplex(obj)
+{
+	if(obj.simplex1.length==2)
+	{
+		return linearsimplex(obj);
+	}
+	return triangularsimplex(obj);
+}
+
+function linearsimplex(obj)
+{
+	let a  = obj.simplex1[1];
+	let b  = obj.simplex1[0];
+	let origin  =  new THREE.Vector3(0,0,0);
+	let ab = subvec(b,a);
+	let ao = subvec(origin,a);
+	let temp = vectortripleprod(ab,ao,ab);
+	obj.simplex1.push(temp);
+	obj.d1 = ab;
+	return false;
+}
+
+function triangularsimplex(obj)
+{
+	let simplex = obj.simplex1;
+	let origin  =  new THREE.Vector3(0,0,0);
+	let d = obj.d1;
+	let c=simplex[0];
+	let b=simplex[1];
+	let a=simplex[2];
+	let ab = subvec(b,a);
+	let ac = subvec(c,a);
+	let ao = subvec(origin,a);
+	let abperp = vectortripleprod(ac,ab,ab);
+	let acperp = vectortripleprod(ab,ac,ac);
+
+	if(abperp.dot(ao)>0)			//origin is in region ab
+	{
+		simplex[0]=b;
+		simplex[1]=a;
+		d = abperp;
+		obj.simplex1 = simplex;
+		obj.d1  = d;
+		return false;
+	}
+
+	else if(acperp.dot(ao)>0)			//origin is in region ac
+	{
+		simplex[0]=c;
+		simplex[1]=a;
+		d = acperp;
+		obj.simplex1 = simplex;
+		obj.d1  = d;
+		return false;
+	}
+
+	return true;
+
+}
+
+function subvec(vec1,vec2){
+	let dx=vec1.x-vec2.x;
+	let dy=vec1.y-vec2.y;
+	let dz=vec1.z-vec2.z;
+	let d = new THREE.Vector3(dx,dy,dz).normalize();
+	return d;
+
+}
+
+function vectortripleprod(vec1,vec2,vec3)         //returns (vec1xvec2)xvec3
+{
+	vec1.cross(vec2);
+	vec1.cross(vec3);
+	return vec1.normalize();
 }
